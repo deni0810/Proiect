71c73adfc9eed30dbb6e3bbca6a218a3b3494c60
Podar Razvan <razvanpodar.pr@gmail.com>
2021-04-13 12:41:41 +0300

    bug fixed

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { IUser } from '../shared/interfaces/user.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private userSubject$ = new BehaviorSubject<IUser>(null!);
+  public user$: Observable<IUser> = this.userSubject$.asObservable();
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -30,5 +35,16 @@ export class AuthService {
 
   handleAuthentification(data: any) {
     localStorage.setItem('userData', JSON.stringify(data));
+  }
+
+  autologin() {
+    const user = JSON.parse(localStorage.getItem('userData')!);
+    this.userSubject$.next(user);
+    //! Sterge asta
+    // Asa o sa folosesti acel user. Mai sus ii atribui o valoare, iar mai jos, o citesti
+    this.user$.subscribe((user) => {
+      console.log(user);
+    });
+    //!
   }
 }
