92aa6bb56fe6a023c7d749845aeea3fc87b4c941
deni0810 <deni.ciobanu@yahoo.com>
2021-04-22 22:42:41 +0300

    autologout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,11 +3,15 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { IUser } from '../shared/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private userSubject$ = new BehaviorSubject<IUser>(null!);
   public user$: Observable<IUser> = this.userSubject$.asObservable();
+  router: any;
+  tokenExpirationTimer: any;
+
 
   constructor(private http: HttpClient) {}
 
@@ -45,7 +49,35 @@ export class AuthService {
     // Asa o sa folosesti acel user. Mai sus ii atribui o valoare, iar mai jos, o citesti
     this.user$.subscribe((user) => {
       console.log(user);
+    //   const expirationDuration =
+    //   new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+    // if (expirationDuration < 0) {
+    //   this.logout();
+      return;
     });
     //!
+
+
   }
+
+  // logout() {
+  //   const user = JSON.parse(localStorage.getItem('userData')!);
+  //   localStorage.removeItem('userData');
+  //   this.router.navigate(['/login']);
+  // }
+
+  //  autologout(expirationDuration:number){
+  //   const user = JSON.parse(localStorage.getItem('userData')!);
+  //   // const expirationDuration =
+  //   //   new Date(user._tokenExpirationDate).getTime() - new Date().getTime();
+  //   //   if (expirationDuration < 0) {
+  //   //     localStorage.removeItem('userData');
+  //   //     return;
+  //   //   }
+  //   setTimeout(() => {
+  //     this.logout();
+  //   }, expirationDuration);
+  //  }
+
+
 }
